4d508e68f1706137aa8589d34cb3b867105bad13
mohammedterfa <mohammedterfa@gmail.com>
2024-02-14 12:07:28 +0300

    make add new item

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function get_all_customers()
+    {
+        $customers = Customer::orderBy('id', 'desc')->get();
+
+        return response()->json([
+           'customers' => $customers
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/get_all_invoices', [InvoiceController::class, 'get_all_invoices']);
 Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
 Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
+Route::get('/get_all_customers', [CustomerController::class, 'get_all_customers']);
